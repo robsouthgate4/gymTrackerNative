2efbe0162ceb241c5d2262828877b8d23548db76
robsouthgate <robsouthgate4@gmail.com>
2017-06-17 17:44:17 +0100

    Initial Commit

---
+++ GymTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GymTracker
+//
+//  Created by Robert Southgate on 17/06/2017.
+//  Copyright © 2017 Robert Southgate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
